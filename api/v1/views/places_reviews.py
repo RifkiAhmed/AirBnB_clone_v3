b5b3bb55568d150b017f7c23477d1de491cd5891
Ikbal_Eddiani <eddianiiqbal@gmail.com>
2023-10-28 15:06:46 -0700

    Creates a Review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Review view"""
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response
+from flask import abort, jsonify, make_response, request
 from models.place import Place
 from models.review import Review
+from models.user import User
 from models import storage
 
 
@@ -38,3 +39,31 @@ def delete_review(review_id):
         return make_response(jsonify({}), 200)
     else:
         abort(404)
+
+
+@app_views.route(
+        '/places/<place_id>/reviews', strict_slashes=False, methods=['POST'])
+def create_review(place_id):
+    """creates a Review object"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    try:
+        data = request.get_json()
+        if 'user_id' not in data:
+            raise KeyError('Missing user_id')
+        user = storage.get(User, data.get('user_id'))
+        if not user:
+            raise ValueError
+        if 'text' not in data:
+            raise KeyError('Missing text')
+        data['place_id'] = place_id
+        review = Review(**data)
+        review.save()
+        return make_response(jsonify(review.to_dict()), 201)
+    except KeyError as error:
+        abort(400, description=error.args[0])
+    except ValueError:
+        abort(404)
+    except Exception:
+        abort(400, description='Not a JSON')
