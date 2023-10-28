fb100fdb176923a6d1d18c35ae515fbea9a69603
Ahmed <rifkiahmed.91@gmail.com>
2023-10-28 16:13:31 +0000

    Fix checks

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,7 +12,7 @@ def get_cities(state_id):
     """retrieves the list of all cities related to a State object"""
     state = storage.get(State, state_id)
     if state:
-        return [city.to_dict() for city in state.cities]
+        return jsonify([city.to_dict() for city in state.cities])
     else:
         abort(404)
 
@@ -45,15 +45,18 @@ def create_city(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    data = request.get_json()
-    if type(data) != dict:
+    try:
+        data = request.get_json()
+        if 'name' not in data:
+            raise KeyError
+        data["state_id"] = state_id
+        city = City(**data)
+        city.save()
+        return make_response(jsonify(city.to_dict()), 201)
+    except KeyError:
+        abort(400, description='Missing name')
+    except Exception:
         abort(400, description='Not a JSON')
-    if 'name' not in data:
-        return abort(400, description='Missing name')
-    data["state_id"] = state_id
-    city = City(**data)
-    city.save()
-    return jsonify(city.to_dict()), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
@@ -61,14 +64,14 @@ def update_city(city_id):
     """updates a City object"""
     ignored_keys = ['id', 'state_id', 'created_at', 'updated_at']
     city = storage.get(City, city_id)
-    if city:
+    if not city:
+        abort(404)
+    try:
         data = request.get_json()
-        if type(data) != dict:
-            return abort(400, {'message': 'Not a JSON'})
         for key, value in data.items():
             if key not in ignored_keys:
                 setattr(city, key, value)
         storage.save()
-        return jsonify(city.to_dict()), 200
-    else:
-        abort(404)
+        return make_response(jsonify(city.to_dict()), 200)
+    except Exception:
+        abort(400, description='Not a JSON')
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,7 +10,7 @@ from models import storage
 def get_states():
     """retrieves the list of all State object"""
     states = storage.all(State)
-    return [state.to_dict() for state in states.values()]
+    return jsonify([state.to_dict() for state in states.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
@@ -37,14 +37,18 @@ def delete_state(state_id):
 
 @app_views.route('/states', strict_slashes=False, methods=['POST'])
 def create_state():
-    data = request.get_json()
-    if type(data) != dict:
-        abort(400, {'message': 'Not a JSON'})
-    if 'name' not in data:
-        return abort(400, {'message': 'Missing name'})
-    state = State(**data)
-    state.save()
-    return jsonify(state.to_dict()), 201
+    """creates a State object"""
+    try:
+        data = request.get_json()
+        if 'name' not in data:
+            raise KeyError
+        state = State(**data)
+        state.save()
+        return make_response(jsonify(state.to_dict()), 201)
+    except KeyError:
+        abort(400, description='Missing name')
+    except Exception:
+        abort(400, description='Not a JSON')
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
@@ -52,14 +56,14 @@ def update_state(state_id):
     """updates a State object"""
     ignored_keys = ['id', 'created_at', 'updated_at']
     state = storage.get(State, state_id)
-    if state:
+    if not state:
+        abort(404)
+    try:
         data = request.get_json()
-        if type(data) != dict:
-            return abort(400, {'message': 'Not a JSON'})
         for key, value in data.items():
             if key not in ignored_keys:
                 setattr(state, key, value)
         storage.save()
-        return jsonify(state.to_dict()), 200
-    else:
-        abort(404)
+        return make_response(jsonify(state.to_dict()), 200)
+    except Exception:
+        abort(400, description='Not a JSON')
